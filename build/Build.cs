65bb8b35bbb10d7fc2d3b2408d2a30a8ce320ba2
Mikhail <mkozlov759@gmail.com>
2020-12-23 14:26:37 +0100

    Changed library version to 0.11.1

--- build/Build.cs
+++ build/Build.cs
@@ -1,7 +1,5 @@
 #region
 
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using Nuke.Common;
@@ -13,7 +11,6 @@ using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Tools.GitHub;
-using Nuke.Common.Tools.GitVersion;
 using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
@@ -41,7 +38,7 @@ class Build : NukeBuild
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
-    const string Version = "0.11.0";
+    const string Version = "0.11.1";
 
     [Solution] readonly Solution Solution;
     [GitRepository] readonly GitRepository GitRepository;
