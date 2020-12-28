957de028e1ebf724606653435dac438abea47231
Mikhail <mkozlov759@gmail.com>
2020-12-28 22:42:07 +0100

    Changed framework version to 0.12.0

--- build/Build.cs
+++ build/Build.cs
@@ -38,7 +38,7 @@ class Build : NukeBuild
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
-    const string Version = "0.11.1";
+    const string Version = "0.12.0";
 
     [Solution] readonly Solution Solution;
     [GitRepository] readonly GitRepository GitRepository;
